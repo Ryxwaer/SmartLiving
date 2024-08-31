28dcff28df683a270214a23e87568981fecef721
Ryxwaer <ryxwaer@gmail.com>
2024-08-31 22:39:40 +0200

    session config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-08-27',
   runtimeConfig: {
     session: {
+      name: "nuxt-session",
+      password: process.env.NUXT_SESSION_PASSWORD || randomBytes(32).toString('hex'),
       maxAge: 60 * 60 * 24 * 30, // 1 month in seconds
     }
   },
